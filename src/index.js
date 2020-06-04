01ec9357e096a588c577620fa531c7cebb9d74fb
kangG13 <dmssk13@gmail.com>
2020-06-04 13:19:47 +0900

    불필요한 파일 삭제

--- src/index.js
+++ src/index.js
@@ -7,22 +7,16 @@ import "assets/css/mycss.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
 
-import Index from "views/Index.js";
 import Home from "views/Home.js";
-import Home_save from "views/Home save.js";
 import Kdkd from "views/Kdkd.js";
 import Sgpg from "views/Sgpg.js";
 import Football from "views/Football.js";
-import Landing from "views/examples/Landing.js";
-import Login from "views/examples/Login.js";
-import Profile from "views/examples/Profile.js";
-import Register from "views/examples/Register.js";
+import Sample from "views/Sample.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" exact render={props => <Home {...props} />} />
-      <Route path="/index" exact render={props => <Index {...props} />} />
       <Route
         path="/kdkd"
         exact
@@ -41,23 +35,12 @@ ReactDOM.render(
       <Route
         path="/home"
         exact
-        render={props => <Home_save {...props} />}
+        render={props => <Home {...props} />}
       />
       <Route
-        path="/landing-page"
+        path="/sample"
         exact
-        render={props => <Landing {...props} />}
-      />
-      <Route path="/login-page" exact render={props => <Login {...props} />} />
-      <Route
-        path="/profile-page"
-        exact
-        render={props => <Profile {...props} />}
-      />
-      <Route
-        path="/register-page"
-        exact
-        render={props => <Register {...props} />}
+        render={props => <Sample {...props} />}
       />
       <Redirect to="/" />
     </Switch>
