df4518e6981470b3cd450ad1de972019207e2ce7
KangG <dmssk13@gmail.com>
2021-10-13 23:08:47 +0900

    webpack 적용

---
+++ webpack.config.build.js
@@ -0,0 +1,72 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.js',
+        chunkFilename: 'static/js/[name].chunk.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(PNG|png|jpg|woff|woff2|eot|ttf|svg|gif|otf)$/,
+                exclude: /node_modules/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            favicon: 'public/favicon.ico'
+        }),
+        new webpack.DefinePlugin({
+            ENV_MODE: JSON.stringify('build')
+        })
+    ],
+    resolve: {
+        modules: [path.resolve(__dirname, './src'), 'node_modules'],
+        extensions: ['.js', '.json', 'css'],
+        alias: {
+            src: path.resolve(__dirname, './src'),
+            assets: path.resolve(__dirname, './src/assets'),
+            components: path.resolve(__dirname, './src/components'),
+            views: path.resolve(__dirname, './src/views')
+        }
+    }
+};
